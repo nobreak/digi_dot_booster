76500162d467808111b2c279ba1925b60ea41dc4
nobreak <github@philipphomann.de>
2017-05-08 09:32:14 -0100

    added byte array and posibility to let decide when to show

--- digiDotBooster.py
+++ digiDotBooster.py
@@ -14,6 +14,7 @@ class DigiDotBooster_LED(object):
         def __init__(self, num):    
             self.countPixels = num
             self.delay = 0.00003 * float(num)
+            self.byteData = []
             # open /dev/spidev0.1 (CS1 Pin wird verwendet)
             self.spi = spidev.SpiDev()
             self.spi.open(0, 1)
@@ -24,40 +25,47 @@ class DigiDotBooster_LED(object):
             time.sleep(0.018)
 
         def show(self):
-            self.spi.writebytes([0xB2])
-            time.sleep(0.4)
+            self.byteData.append(0xB2)
+            self.spi.writebytes(self.byteData)
+            self.byteData[:] = []
+            time.sleep(self.delay)
 
         def numPixels(self):
             return self.countPixels
 
-        def setPixelColor(self, n, color):
+        def setPixelColor(self, n, color, shouldShow=False):
             blue =  color & 255
             green = (color >> 16) & 255
             red =   (color >> 8) & 255
-            self.spi.writebytes([0xA1, red, green, blue, 0xA4, n])
-            time.sleep(self.delay)
+	    self.byteData.extend([0xA1, red, green, blue, 0xA4, n])
+            if (shouldShow):
+              self.show()
 
-        def setPixelColorAll(self, color):
+        def setPixelColorAll(self, color, shouldShow=False):
             blue =  color & 255
             green = (color >> 16) & 255
             red =   (color >> 8) & 255
-            self.spi.writebytes([0xA1, red, green, blue, 0xA5])
-            time.sleep(self.delay)
+            self.byteData.extend([0xA1, red, green, blue, 0xA5])
+            if (shouldShow):
+               self.show()
 
-        def setPixelColorRange(self, color, start, end):
+        def setPixelColorRange(self, start, end, color, shouldShow=False):
             blue =  color & 255
             green = (color >> 16) & 255
             red =   (color >> 8) & 255
-            self.spi.writebytes([0xA1, red, green, blue, 0xA6, start, end])
-            time.sleep(self.delay)    
+            self.byteData.extend([0xA1, red, green, blue, 0xA6, start, end])
+            if (shouldShow):
+               self.show()
 
-        def shiftUpRange(self, count, start, end):    
-            self.spi.writebytes([0xB3, start, end, count])
-            time.sleep(self.delay)
+        def shiftUpRange(self, start, end, count, shouldShow=False):
+            self.byteData.extend([0xB3, start, end, count])
+            if (shouldShow):
+               self.show()
 
-        def shiftDownRange(self, count, start, end):    
-            self.spi.writebytes([0xB4, start, end, count])
-            time.sleep(self.delay)    
+        def shiftDownRange(self, start, end, count, shouldShow=False):    
+            self.byteData.extend([0xB4, start, end, count])
+            if (shouldShow):
+               self.show()               
 
  
         def clear(self):
